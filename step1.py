329685025212e659bc699c37fe47254cda6d35e2
maximus009 <kssrisingavenger@gmail.com>
2015-02-21 10:54:33 +0530

    Kickstart to step1
    Just added the program skeleton

--- step1.py
+++ step1.py
@@ -1,4 +1,33 @@
 ''' 
 Check if the coffeemaker is in the frame or not 
 
+Values will be read from Training files created prior to execution.
+
 '''
+######## Import Declarations ##########
+import cv2
+import numpy
+
+
+#######################################
+
+######## Objects Creation #############
+cap = cv2.VideoCapture(0)
+
+
+#######################################
+
+
+######## Main Loop ####################
+
+while True:
+'''
+Perform Executions
+'''
+
+'''
+Obtained matrix to be sent to step2.py
+'''
+
+
+
